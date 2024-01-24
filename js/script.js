2f4b376caa3e039f8d8e122ee808ec1c912b0dc3
yudai-15 <contact@yudaiweb.online>
2024-01-24 21:06:02 +0900

    blogセクションの追加＆splide+swiperのcssCDMの追加を行ないました。

--- js/script.js
+++ js/script.js
@@ -65,3 +65,25 @@ const splide = new Splide(".splide", {
     },
   },
 }).mount();
+
+// Swiper
+var swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 0,
+  },
+  loop: true,
+  speed: 8000,
+  centeredSlides: true,
+  preventInteractionOnTransition: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
