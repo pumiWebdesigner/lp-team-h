79aabb57326833f39b65ae594709d8bb3c9df5d8
yudai-15 <contact@yudaiweb.online>
2024-01-25 22:31:51 +0900

    splide&予約ボタンのz-indexを修正しています。

--- js/script.js
+++ js/script.js
@@ -51,8 +51,8 @@ const splide = new Splide(".splide", {
   type: "loop", // ループ
   pauseOnHover: false, // カーソルが乗ってもスクロールを停止させない
   pauseOnFocus: false, // 矢印をクリックしてもスクロールを停止させない
-  interval: 2000, // 自動再生の間隔
-  speed: 1000, // スライダーの移動時間
+  interval: 3000, // 自動再生の間隔
+  speed: 2000, // スライダーの移動時間
 
   breakpoints: {
     767: {
