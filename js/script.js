ef9d8e7cc624a442fb0152b49662ed35121dff7f
mitarashi-webcoder <colorful.pini007@gmail.com>
2024-01-27 11:56:09 +0900

    各セクション修正

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ const splide = new Splide(".splide", {
   type: "loop", // ループ
   pauseOnHover: false, // カーソルが乗ってもスクロールを停止させない
   pauseOnFocus: false, // 矢印をクリックしてもスクロールを停止させない
-  interval: 3000, // 自動再生の間隔
+  interval: 4000, // 自動再生の間隔
   speed: 2000, // スライダーの移動時間
   
   breakpoints: {
