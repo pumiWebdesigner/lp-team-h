0a57a497fafecf4b55f26d09c5420a9ab48f44a2
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-01-27 09:51:31 +0900

    別ページ遷移、不要コメント削除

--- js/script.js
+++ js/script.js
@@ -1,28 +1,50 @@
+// ページがロードされた後、URLに#～がある場合はhash有り
+$(document).ready(function () {
+  if (window.location.hash) {
+    var hash = window.location.hash; // #～を取得（例：#access）
+    var elementDistance = $(hash).offset().top; //画面最上部から要素の上端の距離
+    var headerHeight = $(".header").outerHeight(); // ヘッダーの高さ（マージン含む）
+
+    alert(elementDistance);
+    alert(headerHeight);
+
+    // ヘッダーの高さを考慮した位置にスクロール
+    var scrollToPosition = elementDistance - headerHeight;
+    if (hash == "#") {
+      scrollToPosition = 0; // #は初期値0
+    }
+    // スムーズスクロール
+    $("html, body").animate(
+      {
+        scrollTop: scrollToPosition,
+      },
+      300
+    );
+  }
+});
 {
-  // aタグ制御
-  // ⇒ドロワー関連は専用処理
+  // aタグ制御⇒ドロワー関連は専用処理
   jQuery('a[href^="#"]').on("click", function (e) {
-    // ドロワーナビをクリックしたときは、リンク移動後にドロワーを閉じる
-    if (jQuery(this).hasClass("js-drawer__nav--link")) {
-      // リンクへの移動後もドロワーを閉じる処理をしたいので、通常の処理を止める
-      e.preventDefault();
+    // aタグの通常の処理を止める
+    e.preventDefault();
+
+    var id = jQuery(this).attr("href"); // スクロール先のhrefを取得
+    var elementDistance = $(id).offset().top; //画面最上部から要素の上端の距離
+    var headerHeight = $(".header").outerHeight(); // ヘッダーの高さ（マージン含む）
+
+    alert(elementDistance);
+    alert(headerHeight);
+
+    // ヘッダーの高さを考慮した位置にスクロール
+    var scrollToPosition = elementDistance - headerHeight;
+    if (id == "#") {
+      scrollToPosition = 0; // #は初期値0
     }
 
-    // headerの高さを取得（スクロール先は要素の場所よりheader分下の位置）
-    var header = jQuery(".header").innerHeight();
-    // スクロール先のhrefを取得
-    var id = jQuery(this).attr("href");
-
-    // スクロールする距離
-    // #は初期値0,#以外は要素の高さ（headerの高さも考慮）
-    var position = 0;
-    if (id != "#") {
-      position = jQuery(id).offset().top - header;
-    }
-    // href設定のリンクへスクロールする
+    // スムーズスクロール
     jQuery("html,body").animate(
       {
-        scrollTop: position,
+        scrollTop: scrollToPosition,
       },
       300
     );
@@ -74,7 +96,7 @@ const splide = new Splide(".splide", {
   },
 }).mount();
 
-// Swiper
+Swiper;
 var swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 0,
